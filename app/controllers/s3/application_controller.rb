3f875c15d53692e1808fea882bceed7497080c88
Jurriaan Schrofer <jschrofer@gmail.com>
2021-04-06 14:20:59 +0200

    changes for admin auth

--- app/controllers/s3/application_controller.rb
+++ app/controllers/s3/application_controller.rb
@@ -1,11 +1,5 @@
 module S3
   class ApplicationController < ActionController::API
 
-      private
-
-      def slice_params(*args)
-        params.slice(*args).as_json.symbolize_keys
-      end
-
   end
 end
--- app/controllers/s3/records_controller.rb
+++ app/controllers/s3/records_controller.rb
@@ -1,11 +1,17 @@
-module S3
-  class RecordsController < ApplicationController
+class S3::RecordsController < API::Admin::BaseController #ApplicationController
 
+  before_action :authorize_admin
 
-    def deleted_records
-      data = S3::OldDeletedRecordsService.get_records(**slice_params(:db_table, :start_date, :end_date, :env_id, :env_name))
-      render json: {deleted_records: data}
-    end
-
+  def deleted_records
+    data = S3::OldDeletedRecordsService.get_records(**slice_params(:db_table, :start_date, :end_date, :env_id, :env_name))
+    render json: {deleted_records: data}
   end
+
+  private
+
+  def slice_params(*args)
+    params.slice(*args).as_json.symbolize_keys
+  end
+
 end
+
